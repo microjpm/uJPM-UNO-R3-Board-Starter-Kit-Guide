c1560edcb5831b555a01ced8e2452cacf1558861
microjpm <119355831+microjpm@users.noreply.github.com>
2022-11-28 13:34:28 -0600

    Add files via upload

---
+++ Circuit_1A-Blink/Circuit_1A-Blink.ino
@@ -0,0 +1,28 @@
+/*
+  uJPM UNO R3 Board Starter Kit
+  Circuit 1A-Blink
+
+  Turns an LED connected to pin 13 on and off. Repeats forever.
+
+  This sketch was written by MicroJPM, with lots of help from the Arduino community.
+  This code is completely free for any use.
+*/
+
+void setup() {
+
+  pinMode(13, OUTPUT);      // Set pin 13 to output
+
+}
+
+
+void loop() {
+
+  digitalWrite(13, HIGH);   // Turn on the LED
+
+  delay(2000);              // Wait for two seconds
+
+  digitalWrite(13, LOW);    // Turn off the LED
+
+  delay(2000);              // Wait for two seconds
+
+}
